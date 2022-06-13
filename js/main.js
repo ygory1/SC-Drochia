323f48c05d050cf13b076db6f2615d2b7babf997
ygory1 <89040852+ygory1@users.noreply.github.com>
2022-06-13 15:30:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+new SimpleLightbox({ elements: '.container-galerie a'  });
+
+emailjs.init("D9kErHU17r5MsGbhz");
+
+function send() {
+
+    if (document.getElementById("formular").checkValidity() == false) {
+        document.getElementById("formular").reportValidity();
+        return;
+    }
+
+    var date = {
+        nume: document.getElementById("nume").value,
+        email: document.getElementById("email").value,
+        mesaj: document.getElementById("mesaj").value
+    };
+
+    emailjs.send("service_axgq46x", "template_b76hn3a", date)
+        .then(function (raspuns) {
+            alert("Mesajul a fost transmis.")
+            document.getElementById("formular").reset();
+        }, function (error) {
+            alert("Eroare la transmitere. Contactati programatorul.")
+        })
+}
